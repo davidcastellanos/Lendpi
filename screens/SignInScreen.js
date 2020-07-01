1a43972452d8fb54eb6ad2d740db3c75dbb70a8c
Derhks <derhks@gmail.com>
2020-07-01 18:44:45 -0500

    [Update] post user data

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -7,19 +7,12 @@ import {
     Platform,
     StyleSheet ,
     StatusBar,
-    Alert
 } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import LinearGradient from 'react-native-linear-gradient';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import Feather from 'react-native-vector-icons/Feather';
-
 import { useTheme } from 'react-native-paper';
-
-import { AuthContext } from '../components/context';
-
-import Users from '../model/users';
-
+import { AuthContext } from '../components/context'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {
   GoogleSignin,
   GoogleSigninButton,
@@ -28,133 +21,56 @@ import {
 
 const WebClientID = '8133386440-jl74s0b7u2me6mbfogncubunrbjhlfr7.apps.googleusercontent.com';
 
+
 const SignInScreen = ({navigation}) => {
+  const { colors } = useTheme();
+  const { signIn } = React.useContext(AuthContext);
 
-    const [data, setData] = React.useState({
-        username: '',
-        password: '',
-        check_textInputChange: false,
-        secureTextEntry: true,
-        isValidUser: true,
-        isValidPassword: true,
-    });
+  GoogleSignin.configure({
+    webClientId: WebClientID, // client ID of type WEB for your server(needed to verify user ID and offline access)
+    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+    forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+    accountName: '', // [Android] specifies an account name on the device that should be used
+  });
 
-    const { colors } = useTheme();
-
-    const { signIn } = React.useContext(AuthContext);
-
-    const textInputChange = (val) => {
-        if( val.trim().length >= 4 ) {
-            setData({
-                ...data,
-                username: val,
-                check_textInputChange: true,
-                isValidUser: true
-            });
-        } else {
-            setData({
-                ...data,
-                username: val,
-                check_textInputChange: false,
-                isValidUser: false
-            });
-        }
-    }
-
-    const handlePasswordChange = (val) => {
-        if( val.trim().length >= 8 ) {
-            setData({
-                ...data,
-                password: val,
-                isValidPassword: true
-            });
-        } else {
-            setData({
-                ...data,
-                password: val,
-                isValidPassword: false
-            });
-        }
-    }
-
-    const updateSecureTextEntry = () => {
-        setData({
-            ...data,
-            secureTextEntry: !data.secureTextEntry
-        });
-    }
-
-    const handleValidUser = (val) => {
-        if( val.trim().length >= 4 ) {
-            setData({
-                ...data,
-                isValidUser: true
-            });
-        } else {
-            setData({
-                ...data,
-                isValidUser: false
-            });
-        }
-    }
-
-    const loginHandle = (userName, password) => {
-
-        const foundUser = Users.filter( item => {
-            return userName == item.username && password == item.password;
-        } );
-
-        if ( data.username.length == 0 || data.password.length == 0 ) {
-            Alert.alert('Entrada incorrecta!', 'El campo de nombre de usuario o contraseña no puede estar vacío.', [
-                {text: 'Okay'}
-            ]);
-            return;
-        }
-
-        if ( foundUser.length == 0 ) {
-            Alert.alert('Error User o Pass!', 'Nombre de usuario o contraseña incorrecta.', [
-                {text: 'Okay'}
-            ]);
-            return;
-        }
-        signIn(foundUser);
-    }
-
-    GoogleSignin.configure({
-        webClientId: WebClientID, // client ID of type WEB for your server(needed to verify user ID and offline access)
-        offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-        forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
-        accountName: '', // [Android] specifies an account name on the device that should be used
-      });
-
-    const googleSignIn = async () => {
-      try {
-        await GoogleSignin.hasPlayServices();
-        const info = await GoogleSignin.signIn();
-        const listData = [info];
-        console.warn({userInfo: info});
-        console.warn(info.user.name);
-        console.warn('listData ->', listData);
-        signIn(listData);
-        setUserInfo(info);
-      } catch (error) {
-        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-          // user cancelled the login flow
-        } else if (error.code === statusCodes.IN_PROGRESS) {
-          // operation (e.g. sign in) is in progress already
-        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-          // play services not available or outdated
-        } else {
-          // some other error happened
-        }
+  const googleSignIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const info = await GoogleSignin.signIn();
+      await fetch('https://lendpi-gateway.herokuapp.com/api-gateway/new-worker', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        first_name: info.user.givenName,
+        last_name: info.user.familyName,
+        email: info.user.email,
+        token: info.idToken,
+        photo: info.user.photo
+      })
+    })
+      console.warn('Testing ->', info.user.givenName, info.user.email, info.idToken);
+      signIn([info]);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
       }
-    };
+    }
+  };
 
-    return (
-      <View style={styles.container}>
-          <StatusBar backgroundColor='#FA5C61' barStyle="light-content"/>
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor='#FA5C61' barStyle="light-content"/>
         <View style={styles.header}>
-            <Text style={styles.text_header}>Inicia Sesión</Text>
+            <Text style={styles.text_header}>Crowdlending by Rappi</Text>
         </View>
         <Animatable.View
             animation="fadeInUpBig"
@@ -162,112 +78,25 @@ const SignInScreen = ({navigation}) => {
                 backgroundColor: colors.background
             }]}
         >
-            <Text style={[styles.text_footer, {
-                color: colors.text
-            }]}>Usuario</Text>
-            <View style={styles.action}>
-                <FontAwesome
-                    name="user-o"
-                    color={colors.text}
-                    size={20}
-                />
-                <TextInput
-                    placeholder="Coloca tu Usuario"
-                    placeholderTextColor="#666666"
-                    style={[styles.textInput, {
-                        color: colors.text
-                    }]}
-                    autoCapitalize="none"
-                    onChangeText={(val) => textInputChange(val)}
-                    onEndEditing={(e)=>handleValidUser(e.nativeEvent.text)}
-                />
-                {data.check_textInputChange ?
-                <Animatable.View
-                    animation="bounceIn"
-                >
-                    <Feather
-                        name="check-circle"
-                        color="green"
-                        size={20}
-                    />
-                </Animatable.View>
-                : null}
-            </View>
-            { data.isValidUser ? null :
-            <Animatable.View animation="fadeInLeft" duration={500}>
-            <Text style={styles.errorMsg}>El nombre de usuario debe tener 4 o más caracteres.</Text>
-            </Animatable.View>
-            }
-
-
-            <Text style={[styles.text_footer, {
-                color: colors.text,
-                marginTop: 35
-            }]}>Password</Text>
-            <View style={styles.action}>
-                <Feather
-                    name="lock"
-                    color={colors.text}
-                    size={20}
-                />
-                <TextInput
-                    placeholder="Coloca tu Password"
-                    placeholderTextColor="#666666"
-                    secureTextEntry={data.secureTextEntry ? true : false}
-                    style={[styles.textInput, {
-                        color: colors.text
-                    }]}
-                    autoCapitalize="none"
-                    onChangeText={(val) => handlePasswordChange(val)}
-                />
-                <TouchableOpacity
-                    onPress={updateSecureTextEntry}
-                >
-                    {data.secureTextEntry ?
-                    <Feather
-                        name="eye-off"
-                        color="grey"
-                        size={20}
-                    />
-                    :
-                    <Feather
-                        name="eye"
-                        color="grey"
-                        size={20}
-                    />
-                    }
-                </TouchableOpacity>
-            </View>
-            { data.isValidPassword ? null :
-            <Animatable.View animation="fadeInLeft" duration={500}>
-            <Text style={styles.errorMsg}>La contraseña debe tener 8 o más caracteres.</Text>
-            </Animatable.View>
-            }
-
-
-            <TouchableOpacity>
-                <Text style={{color: '#FF9295', marginTop:15}}>Perdiste el password?</Text>
-            </TouchableOpacity>
-            <View style={styles.button}>
-                <TouchableOpacity
-                    style={styles.signIn}
-                    onPress={() => {loginHandle( data.username, data.password )}}
-                >
-                <LinearGradient
-                    colors={['#FF9295', '#FA5C61']}
-                    style={styles.signIn}
-                >
-                    <Text style={[styles.textSign, {
-                        color:'#fff'
-                    }]}>Sign In</Text>
-                </LinearGradient>
-                </TouchableOpacity>
-
-                <GoogleSigninButton
-                  style={{ width: 48, height: 48 }}
-                  size={GoogleSigninButton.Size.Wide}
-                  onPress={googleSignIn} />
-            </View>
+          <View style={styles.button}>
+            <GoogleSigninButton
+              style={{ width: 180, height: 60 }}
+              size={GoogleSigninButton.Size.Wide}
+              onPress={googleSignIn} />
+          </View>
+          <TouchableOpacity onPress={()=>navigation.navigate('SplashScreen')}>
+            <LinearGradient
+              colors={['#FF9295', '#FA5C61']}
+              style={styles.signIn}
+            >
+            <MaterialIcons
+              name="navigate-before"
+              color="#fff"
+              size={20}
+            />
+            <Text style={styles.textSign}>Volver</Text>
+            </LinearGradient>
+          </TouchableOpacity>
         </Animatable.View>
       </View>
     );
@@ -281,65 +110,38 @@ const styles = StyleSheet.create({
       backgroundColor: '#FA5C61'
     },
     header: {
-        flex: 1,
-        justifyContent: 'flex-end',
+        flex: 3.5,
+        justifyContent: 'center',
         paddingHorizontal: 20,
-        paddingBottom: 50
     },
     footer: {
-        flex: 3,
+        flex: 2,
         backgroundColor: '#fff',
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        paddingHorizontal: 20,
-        paddingVertical: 30
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
+        paddingHorizontal: 30
     },
     text_header: {
         color: '#fff',
         fontWeight: 'bold',
         fontSize: 30
     },
-    text_footer: {
-        color: '#05375a',
-        fontSize: 18
-    },
-    action: {
-        flexDirection: 'row',
-        marginTop: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#f2f2f2',
-        paddingBottom: 5
-    },
-    actionError: {
-        flexDirection: 'row',
-        marginTop: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#FF0000',
-        paddingBottom: 5
-    },
-    textInput: {
-        flex: 1,
-        marginTop: Platform.OS === 'ios' ? 0 : -12,
-        paddingLeft: 10,
-        color: '#05375a',
-    },
-    errorMsg: {
-        color: '#FF0000',
-        fontSize: 14,
-    },
     button: {
         alignItems: 'center',
-        marginTop: 50
+        marginTop: 30,
+        marginBottom: -30
     },
     signIn: {
-        width: '100%',
-        height: 50,
+        width: 150,
+        height: 40,
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 10
+        borderRadius: 50,
+        flexDirection: 'row',
+        marginTop: 70
     },
     textSign: {
-        fontSize: 18,
+        color: 'white',
         fontWeight: 'bold'
     }
   });
