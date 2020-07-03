2e88d3f452908705c6bbbcc0f0f056733d09b387
Derhks <derhks@gmail.com>
2020-07-03 15:08:59 -0500

    [Fix] si ya tiene una solicitud no podrá publicar una nueva

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,93 +1,89 @@
-import { TextInput, View, ScrollView, Button, StyleSheet } from 'react-native';
+import { View, ScrollView, Button, StyleSheet, Image, Text, TouchableOpacity, Alert } from 'react-native';
 import * as Animatable from 'react-native-animatable';
-import React, { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const HomeScreen = ({navigation}) => {
-  const { register, handleSubmit, setValue } = useForm();
-  const onSubmit = async data => {
-    await fetch('https://lendpi-gateway.herokuapp.com/api-gateway/new-solicitud', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      id_user: 1098707946,
-      tipo_producto: data.producto,
-      marca: data.marca,
-      modelo: data.modelo,
-      year_model: data.year_model,
-      tiempo_financiacion: data.lend_time,
-      valor_financiacion: data.lend_value
-    })
-  })
-};
+  const urlPhotoUser = 'https://lh3.googleusercontent.com/a-/AOh14GgHJIySggFeLFc4LPmnDSVY4FHX2dd-TYz3o-ODGH0=s96-c';
+  const nameUser = 'Julián Sandoval';
 
-  useEffect(() => {
-  register("producto");
-  register("marca");
-  register("modelo");
-  register("year_model");
-  register("lend_time");
-  register("lend_value");
-}, [register]);
+  const checkSolicitud = async () => {
+    const idUser = 1098707946;
+    const urlSolicitudUser = 'https://database-lendpi.herokuapp.com/solicitudes/solicitud/';
+    const response = await fetch(urlSolicitudUser + idUser);
+    const data = await response.json();
+    if (data.solicitud.length < 1) {
+      navigation.navigate('SolicitudScreen');
+    } else {
+      Alert.alert(
+        "ACCESO DENEGADO",
+        "No tiene ninguna solicitud en proceso, puede realizar una solicitud en la opción de arriba Solicitar Financiación",
+        [
+          { text: "OK" }
+        ],
+        { cancelable: false }
+      );
+    };
+  };
+
+  const checkEstado = async () => {
+    const idUser = 1098707946;
+    const urlSolicitudUser = 'https://database-lendpi.herokuapp.com/solicitudes/solicitud/';
+    const response = await fetch(urlSolicitudUser + idUser);
+    const data = await response.json();
+    if (data.solicitud.length === 1) {
+      navigation.navigate('EstadoScreen');
+    } else {
+      Alert.alert(
+        "ACCESO DENEGADO",
+        "Ya tienes una solicitud en proceso, hasta que esta solicitud no finalice no puede realizar una nueva solocitud",
+        [
+          { text: "OK" }
+        ],
+        { cancelable: false }
+      );
+    };
+  };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View>
-        <Animatable.Image
-            animation="bounceIn"
-            duraton="1500"
-            source={require('../assets/img2.png')}
-            style={styles.logo}
-            resizeMode="stretch"
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: urlPhotoUser,
+          }}
         />
+        <Text style={styles.textHi}>Hola,</Text>
+        <Text style={styles.textPhoto}>{ nameUser }</Text>
+        <TouchableOpacity onPress={checkSolicitud}>
+          <LinearGradient
+            colors={['white', 'white']}
+            style={styles.lend}
+          >
+          <MaterialIcons
+            name="attach-money"
+            color="#FA5C61"
+            size={24}
+          />
+          <Text style={styles.textButton}>Solicitar Financiación</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={checkEstado}>
+          <LinearGradient
+            colors={['white', 'white']}
+            style={styles.lend}
+          >
+          <MaterialIcons
+            name="favorite"
+            color="#FA5C61"
+            size={24}
+          />
+          <Text style={styles.textButton}>Estado Solicitud</Text>
+          </LinearGradient>
+        </TouchableOpacity>
       </View>
-
-      <TextInput
-        placeholder="Producto"
-        style={styles.input}
-        autoCapitalize="none"
-        onChangeText={text => setValue('producto', text)}
-      />
-      <TextInput
-        placeholder="Marca"
-        style={styles.input}
-        autoCapitalize="none"
-        onChangeText={text => setValue('marca', text)}
-      />
-      <TextInput
-        placeholder="Modelo"
-        style={styles.input}
-        autoCapitalize="none"
-        onChangeText={text => setValue('modelo', text)}
-      />
-      <TextInput
-        placeholder="Año Modelo"
-        style={styles.input}
-        autoCapitalize="none"
-        onChangeText={text => setValue('year_model', text)}
-      />
-      <TextInput
-        placeholder="Tiempo de Financiación"
-        style={styles.input}
-        autoCapitalize="none"
-        onChangeText={text => setValue('lend_time', text)}
-      />
-      <TextInput
-        placeholder="Valor a Financiar"
-        style={styles.input}
-        autoCapitalize="none"
-        onChangeText={text => setValue('lend_value', text)}
-      />
-
-      <Button
-        title='confirmar'
-        onPress={handleSubmit(onSubmit)}>
-        Confirmar
-      </Button>
-
     </ScrollView>
     )
 };
@@ -96,15 +92,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+  },
+  tinyLogo: {
+    width: 120,
+    height: 120,
+    flexDirection: 'column',
+    marginTop: 50,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 10,
+    alignSelf: 'center',
+  },
+  lend: {
+    width: 250,
+    height: 90,
+    alignItems: 'center',
     justifyContent: 'center',
-  },
-  input: {
-    height: 40,
-    width: 300,
-    borderColor: 'gray',
+    borderRadius: 10,
+    flexDirection: 'column',
+    marginTop: 50,
     borderWidth: 1,
-    marginTop: 20,
+    borderColor: '#E0E0E0'
   },
+  textButton: {
+    color: '#FA5C61',
+    fontWeight: 'bold',
+    fontSize: 21,
+  },
+  textPhoto: {
+    color: '#FA5C61',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontSize: 21,
+  },
+  textHi: {
+    alignSelf: 'center',
+    color: '#FA5C61',
+    marginTop: 10,
+  }
 });
 
 export default HomeScreen;
