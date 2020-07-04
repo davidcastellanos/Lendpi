8505f12060fe7b16777ae01bf0d8f5364c3b5e5f
Derhks <derhks@gmail.com>
2020-07-04 08:44:15 -0500

    [Change] se asigna una tabview para el historial de inversiones

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,91 +1,59 @@
-import { View, ScrollView, Button, StyleSheet, Image, Text, TouchableOpacity, Alert } from 'react-native';
-import * as Animatable from 'react-native-animatable';
 import React from 'react';
+import { Card, Title } from 'react-native-paper';
 import LinearGradient from 'react-native-linear-gradient';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { ScrollView, Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 const HomeScreen = ({navigation}) => {
   const urlPhotoUser = 'https://lh3.googleusercontent.com/a-/AOh14GgHJIySggFeLFc4LPmnDSVY4FHX2dd-TYz3o-ODGH0=s96-c';
   const nameUser = 'Julián Sandoval';
 
-  const checkSolicitud = async () => {
-    const idUser = 1098707946;
-    const urlSolicitudUser = 'https://database-lendpi.herokuapp.com/solicitudes/solicitud/';
-    const response = await fetch(urlSolicitudUser + idUser);
-    const data = await response.json();
-    if (data.solicitud.length < 1) {
-      navigation.navigate('SolicitudScreen');
-    } else {
-      Alert.alert(
-        "ACCESO DENEGADO",
-        "No tiene ninguna solicitud en proceso, puede realizar una solicitud en la opción de arriba Solicitar Financiación",
-        [
-          { text: "OK" }
-        ],
-        { cancelable: false }
-      );
-    };
-  };
-
-  const checkEstado = async () => {
-    const idUser = 1098707946;
-    const urlSolicitudUser = 'https://database-lendpi.herokuapp.com/solicitudes/solicitud/';
-    const response = await fetch(urlSolicitudUser + idUser);
-    const data = await response.json();
-    if (data.solicitud.length === 1) {
-      navigation.navigate('EstadoScreen');
-    } else {
-      Alert.alert(
-        "ACCESO DENEGADO",
-        "Ya tienes una solicitud en proceso, hasta que esta solicitud no finalice no puede realizar una nueva solocitud",
-        [
-          { text: "OK" }
-        ],
-        { cancelable: false }
-      );
-    };
-  };
-
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <View>
-        <Image
-          style={styles.tinyLogo}
-          source={{
-            uri: urlPhotoUser,
-          }}
-        />
-        <Text style={styles.textHi}>Hola,</Text>
-        <Text style={styles.textPhoto}>{ nameUser }</Text>
-        <TouchableOpacity onPress={checkSolicitud}>
-          <LinearGradient
-            colors={['white', 'white']}
-            style={styles.lend}
-          >
-          <MaterialIcons
-            name="attach-money"
-            color="#FA5C61"
-            size={24}
-          />
-          <Text style={styles.textButton}>Solicitar Financiación</Text>
-          </LinearGradient>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={checkEstado}>
-          <LinearGradient
-            colors={['white', 'white']}
-            style={styles.lend}
-          >
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: urlPhotoUser,
+        }}
+      />
+      <Text style={styles.textHi}>Hola,</Text>
+      <Text style={styles.textPhoto}>{ nameUser }</Text>
+      <Card style={styles.card}>
+        <Card.Content>
+          <Title style={styles.cardTitle}>Tu saldo</Title>
+          <View style={{
+            flexDirection: 'row',
+            marginTop: 15,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+            <MaterialIcons
+              name="favorite"
+              size={24}
+              style={{
+                color: "#FA5C61",
+                marginRight: 10,
+              }}
+            />
+            <Text style={{ fontSize: 20 }}>180.000</Text>
+          </View>
+        </Card.Content>
+      </Card>
+      <TouchableOpacity onPress={()=>navigation.navigate('ProductosScreen')}>
+        <LinearGradient
+          colors={['white', 'white']}
+          style={styles.lend}
+        >
           <MaterialIcons
             name="favorite"
             color="#FA5C61"
             size={24}
           />
-          <Text style={styles.textButton}>Estado Solicitud</Text>
-          </LinearGradient>
-        </TouchableOpacity>
-      </View>
+          <Text style={styles.textButtom}>Invertir</Text>
+        </LinearGradient>
+      </TouchableOpacity>
     </ScrollView>
-    )
+  )
 };
 
 const styles = StyleSheet.create({
@@ -103,21 +71,10 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 10,
     alignSelf: 'center',
   },
-  lend: {
-    width: 250,
-    height: 90,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    flexDirection: 'column',
-    marginTop: 50,
-    borderWidth: 1,
-    borderColor: '#E0E0E0'
-  },
-  textButton: {
+  textHi: {
+    alignSelf: 'center',
     color: '#FA5C61',
-    fontWeight: 'bold',
-    fontSize: 21,
+    marginTop: 10,
   },
   textPhoto: {
     color: '#FA5C61',
@@ -125,11 +82,48 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 21,
   },
-  textHi: {
+  card: {
+    marginTop: 30,
+    width: 250,
+    height: 120,
     alignSelf: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+    borderRadius: 20,
+  },
+  cardTitle: {
+    fontSize: 16,
+  },
+  lend: {
+    width: 170,
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    flexDirection: 'column',
+    marginTop: 50,
+    borderWidth: 1,
+    borderColor: '#E0E0E0',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+  },
+  textButtom: {
     color: '#FA5C61',
-    marginTop: 10,
-  }
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
 });
 
 export default HomeScreen;
